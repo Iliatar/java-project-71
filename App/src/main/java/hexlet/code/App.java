c33e14dcb3568da1a6f3bc4b26671a65743363ba
Iliatar <a.u.tarasov@gmail.com>
2024-04-03 22:56:32 +0300

    Add picocli minimal support

--- App/src/main/java/hexlet/code/App.java
+++ App/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "App", mixinStandardHelpOptions = true, version = "App 0.1", description = "Unknown Application")
+public class App implements Callable<Integer> {
     public static void main (String[] args) {
-        System.out.println("Hello, Porject-71!");
+        int exitCode = new picocli.CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello, project-71!");
+        return 1;
     }
 }
