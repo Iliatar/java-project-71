f716eece269acf9ed9ecd7aeb67ad3622f457c69
Iliatar <a.u.tarasov@gmail.com>
2024-04-04 07:22:45 +0300

    add format option

--- App/src/main/java/hexlet/code/App.java
+++ App/src/main/java/hexlet/code/App.java
@@ -6,6 +6,8 @@ import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "App", mixinStandardHelpOptions = true, version = "App 0.1", description = "Unknown Application")
 public class App implements Callable<Integer> {
+    @CommandLine.Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format", description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
     public static void main (String[] args) {
         int exitCode = new picocli.CommandLine(new App()).execute(args);
         System.exit(exitCode);
