69effed75bef1a5749ff797bf2a2cbb50038f5e6
Iliatar <a.u.tarasov@gmail.com>
2024-04-09 21:16:11 +0300

    add testcoverage badge

--- App/src/test/java/hexlet/code/DifferTest.java
+++ App/src/test/java/hexlet/code/DifferTest.java
@@ -1,38 +1,38 @@
 package hexlet.code;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class DifferTest {
     @Test
-    public void test () throws Exception {
-        String expected1 = "{\n" +
-                "  - follow: false\n" +
-                "  + followed: true\n" +
-                "    host: hexlet.io\n" +
-                "  + login: blablator\n" +
-                "    proxy: 123.234.53.22\n" +
-                "  - timeout: 55\n" +
-                "  + timeout: 200\n" +
-                "}";
+    public void test() throws Exception {
+        String expected1 = "{\n"
+                + "  - follow: false\n"
+                + "  + followed: true\n"
+                + "    host: hexlet.io\n"
+                + "  + login: blablator\n"
+                + "    proxy: 123.234.53.22\n"
+                + "  - timeout: 55\n"
+                + "  + timeout: 200\n"
+                + "}";
         String actual1 = Differ.generate("src/test/resources/json1.json", "src/test/resources/json2.json");
         assertEquals(expected1, actual1);
 
-        String expected2 = "{\n" +
-                "  - follow: false\n" +
-                "  - host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 55\n" +
-                "}";
+        String expected2 = "{\n"
+                + "  - follow: false\n"
+                + "  - host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 55\n"
+                + "}";
         String actual2 = Differ.generate("src/test/resources/json1.json", "src/test/resources/json3");
         assertEquals(expected2, actual2);
 
-        String expected3 = "{\n" +
-                "  + follow: false\n" +
-                "  + host: hexlet.io\n" +
-                "  + proxy: 123.234.53.22\n" +
-                "  + timeout: 55\n" +
-                "}";
+        String expected3 = "{\n"
+                +  "  + follow: false\n"
+                +  "  + host: hexlet.io\n"
+                +  "  + proxy: 123.234.53.22\n"
+                +  "  + timeout: 55\n"
+                +  "}";
         String actual3 = Differ.generate("src/test/resources/json3", "src/test/resources/json1.json");
         assertEquals(expected3, actual3);
     }
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 [![Actions Status](https://github.com/Iliatar/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Iliatar/java-project-71/actions)
 [![Action Status](https://github.com/Iliatar/java-project-71/actions/workflows/main.yml/badge.svg)](https://github.com/Iliatar/java-project-71/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/bbe1f23e1ca2b6df02cd/maintainability)](https://codeclimate.com/github/Iliatar/java-project-71/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/bbe1f23e1ca2b6df02cd/test_coverage)](https://codeclimate.com/github/Iliatar/java-project-71/test_coverage)
 ###Description
 Application compare two plain json files and shows differences between them
 ###Aciinema
