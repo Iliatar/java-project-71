9d90a2529051994723fd2cbe863a531068435b0f
Iliatar <a.u.tarasov@gmail.com>
2024-04-09 20:57:35 +0300

    move test files to test/resources folder

--- App/src/test/java/hexlet/code/DifferTest.java
+++ App/src/test/java/hexlet/code/DifferTest.java
@@ -12,28 +12,28 @@ public class DifferTest {
                 "    host: hexlet.io\n" +
                 "  + login: blablator\n" +
                 "    proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
+                "  - timeout: 55\n" +
                 "  + timeout: 200\n" +
                 "}";
-        String actual1 = Differ.generate("json1.json", "json2.json");
+        String actual1 = Differ.generate("src/test/resources/json1.json", "src/test/resources/json2.json");
         assertEquals(expected1, actual1);
 
         String expected2 = "{\n" +
                 "  - follow: false\n" +
                 "  - host: hexlet.io\n" +
                 "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
+                "  - timeout: 55\n" +
                 "}";
-        String actual2 = Differ.generate("json1.json", "json3");
+        String actual2 = Differ.generate("src/test/resources/json1.json", "src/test/resources/json3");
         assertEquals(expected2, actual2);
 
         String expected3 = "{\n" +
                 "  + follow: false\n" +
                 "  + host: hexlet.io\n" +
                 "  + proxy: 123.234.53.22\n" +
-                "  + timeout: 50\n" +
+                "  + timeout: 55\n" +
                 "}";
-        String actual3 = Differ.generate("json3", "json1.json");
+        String actual3 = Differ.generate("src/test/resources/json3", "src/test/resources/json1.json");
         assertEquals(expected3, actual3);
     }
 }
