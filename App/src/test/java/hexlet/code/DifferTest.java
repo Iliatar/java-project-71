a4e2d0a1b785e15f93a3703603c1df6680d94c4b
Iliatar <a.u.tarasov@gmail.com>
2024-04-09 20:51:22 +0300

    add more  tests

--- App/src/test/java/hexlet/code/DifferTest.java
+++ App/src/test/java/hexlet/code/DifferTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class DifferTest {
     @Test
     public void test () throws Exception {
-        String expected = "{\n" +
+        String expected1 = "{\n" +
                 "  - follow: false\n" +
                 "  + followed: true\n" +
                 "    host: hexlet.io\n" +
@@ -15,8 +15,25 @@ public class DifferTest {
                 "  - timeout: 50\n" +
                 "  + timeout: 200\n" +
                 "}";
-        String actual = Differ.generate("json1.json", "json2.json");
-        assertEquals(expected, actual);
-    }
+        String actual1 = Differ.generate("json1.json", "json2.json");
+        assertEquals(expected1, actual1);
 
+        String expected2 = "{\n" +
+                "  - follow: false\n" +
+                "  - host: hexlet.io\n" +
+                "  - proxy: 123.234.53.22\n" +
+                "  - timeout: 50\n" +
+                "}";
+        String actual2 = Differ.generate("json1.json", "json3");
+        assertEquals(expected2, actual2);
+
+        String expected3 = "{\n" +
+                "  + follow: false\n" +
+                "  + host: hexlet.io\n" +
+                "  + proxy: 123.234.53.22\n" +
+                "  + timeout: 50\n" +
+                "}";
+        String actual3 = Differ.generate("json3", "json1.json");
+        assertEquals(expected3, actual3);
+    }
 }
