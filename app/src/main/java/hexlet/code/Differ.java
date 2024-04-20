6188134a1589f0b1379631dfe857206885ba3c74
Iliatar <a.u.tarasov@gmail.com>
2024-04-20 09:21:22 +0300

    refactor parser

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,11 +1,14 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2, String formatterName) throws Exception {
-        Map<String, Object> map1 = Parser.getMap(filepath1);
-        Map<String, Object> map2 = Parser.getMap(filepath2);
+        Map<String, Object> map1 = parse(filepath1);
+        Map<String, Object> map2 = parse(filepath2);
 
         Map<String, Map<String, Object>> paramsDiffer = Comparer.getDifference(map1, map2);
 
@@ -14,5 +17,14 @@ public class Differ {
 
     public static String generate(String filepath1, String filepath2) throws Exception {
         return generate(filepath1, filepath2, "stylish");
+    }
+
+    private static Map<String, Object> parse(String filePath) throws Exception {
+        Path path = Paths.get(filePath);
+        String fileData = Files.readString(path);
+        String format = filePath.substring(filePath.indexOf('.') + 1);
+
+
+        return Parser.parse(fileData, format);
     }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -22,4 +22,28 @@ public class Parser {
         }
         return objectMapper.readValue(fileData, new TypeReference<>() { });
     }
+
+    public static Map<String, Object> parse(String content, String dataFormat) throws Exception {
+        switch (dataFormat) {
+            case "yml":
+            case "yaml":
+                return parseYaml(content);
+            case "json":
+                return parseJson(content);
+            default:
+                // Исключения еще не проходились
+                // В принципе на этом этапе достаточно, что тут будет обычный break
+                throw new Exception("Unknown format: '" + dataFormat + "'");
+        }
+    }
+
+    private static Map<String, Object> parseYaml(String content) throws Exception  {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(content, Map.class);
+    }
+
+    private static Map<String, Object> parseJson(String content) throws Exception  {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, Map.class);
+    }
 }
