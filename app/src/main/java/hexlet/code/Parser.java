199d13cd68362913b92337bd0ef80962a472f12a
Iliatar <a.u.tarasov@gmail.com>
2024-04-24 07:20:04 +0300

    remove getMap method from Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,28 +1,11 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> getMap(String filePath) throws Exception {
-        Path path = Paths.get(filePath);
-        String fileData = Files.readString(path);
-
-        ObjectMapper objectMapper;
-        if (filePath.endsWith("yml")) {
-            objectMapper = new ObjectMapper(new YAMLFactory());
-        } else {
-            objectMapper = new ObjectMapper();
-        }
-        return objectMapper.readValue(fileData, new TypeReference<>() { });
-    }
-
     public static Map<String, Object> parse(String content, String dataFormat) throws Exception {
         switch (dataFormat) {
             case "yml":
