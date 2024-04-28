0c726746e72b5dec759dc98bbec829d8deadb39e
Iliatar <a.u.tarasov@gmail.com>
2024-04-28 09:34:04 +0300

    refactor test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -5,7 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -28,7 +29,7 @@ public final class DifferTest {
 
     private static String readFixture(String fileName) throws Exception {
         Path filePath = getFixturePath(fileName);
-        return Files.readString(filePath);
+        return Files.readString(filePath).trim();
     }
 
     @BeforeAll
@@ -43,60 +44,40 @@ public final class DifferTest {
         System.setOut(new PrintStream(output));
     }
 
-    @Test
-    public void testJsonJson() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/jsonTree1.json",
-                "src/test/resources/jsonTree2.json", "json");
-        assertEquals(resultJson, actual1);
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTestStylish(String format) throws Exception {
+        String filePath1 = getFixturePath("tree1." + format).toString();
+        String filePath2 = getFixturePath("tree2." + format).toString();
+
+        assertEquals(resultStylish, Differ.generate(filePath1, filePath2, "stylish"));
     }
 
-    @Test
-    public void testYamlJson() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/yamlTree1.yml",
-                "src/test/resources/yamlTree2.yml", "json");
-        assertEquals(resultJson, actual1);
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTestDefault(String format) throws Exception {
+        String filePath1 = getFixturePath("tree1." + format).toString();
+        String filePath2 = getFixturePath("tree2." + format).toString();
+
+        assertEquals(resultStylish, Differ.generate(filePath1, filePath2));
     }
 
-    @Test
-    public void testJsonStylish() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/jsonTree1.json",
-                "src/test/resources/jsonTree2.json", "stylish");
-        assertEquals(resultStylish, actual1);
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTestPlain(String format) throws Exception {
+        String filePath1 = getFixturePath("tree1." + format).toString();
+        String filePath2 = getFixturePath("tree2." + format).toString();
+
+        assertEquals(resultPlain, Differ.generate(filePath1, filePath2, "plain"));
     }
 
-    @Test
-    public void testYamlStylish() throws Exception {
-        String actual2 = Differ.generate("src/test/resources/yamlTree1.yml",
-                "src/test/resources/yamlTree2.yml", "stylish");
-        assertEquals(resultStylish, actual2);
-    }
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTestJson(String format) throws Exception {
+        String filePath1 = getFixturePath("tree1." + format).toString();
+        String filePath2 = getFixturePath("tree2." + format).toString();
 
-    @Test
-    public void testJsonPlain() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/jsonTree1.json",
-                "src/test/resources/jsonTree2.json", "plain");
-        assertEquals(resultPlain, actual1);
-    }
-
-    @Test
-    public void testYamlPlain() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/yamlTree1.yml",
-                "src/test/resources/yamlTree2.yml", "plain");
-        assertEquals(resultPlain, actual1);
-    }
-
-    @Test
-    public void testJsonDefault() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/jsonTree1.json",
-                "src/test/resources/jsonTree2.json");
-        assertEquals(resultStylish, actual1);
-    }
-
-    @Test
-    public void testYamlDefault() throws Exception {
-        String actual1 = Differ.generate("src/test/resources/yamlTree1.yml",
-                "src/test/resources/yamlTree2.yml");
-        assertEquals(resultStylish, actual1);
+        assertEquals(resultJson, Differ.generate(filePath1, filePath2, "json"));
     }
 
     @AfterEach
